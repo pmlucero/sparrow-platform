65d37da5af41cdb2ff6669afd295b54d53f2304c
Pablo Lucero <pablo.lucero@globallogic.com>
2018-07-29 16:56:12 -0500

    Import initial

---
+++ src/main/java/ar/com/zumma/sparrowplatform/SparrowPlatformApplication.java
@@ -0,0 +1,12 @@
+package ar.com.zumma.sparrowplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparrowPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparrowPlatformApplication.class, args);
+	}
+}
