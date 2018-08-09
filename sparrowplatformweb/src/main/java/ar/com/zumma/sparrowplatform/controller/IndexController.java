6d3a1f3c452ea8a0cb3165b3b0276295ac285361
Pablo Lucero <pablo.lucero@globallogic.com>
2018-08-09 18:27:42 -0300

    Index page basics & controller - closes #12

---
+++ sparrowplatformweb/src/main/java/ar/com/zumma/sparrowplatform/controller/IndexController.java
@@ -0,0 +1,14 @@
+package ar.com.zumma.sparrowplatform.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index.html"})
+    public String index() {
+
+        return "index";
+    }
+}
